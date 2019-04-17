17cdd94992b9177b321e6306fe4e9e90cd8b8b55
Terry <B00100763@student.itb.ie>
2019-04-17 21:35:01 +0100

    Placedby now working upon cart commit using sessions

--- src/Controller/Backend.php
+++ src/Controller/Backend.php
@@ -5,17 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Login;
+use App\Entity\Orders;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Backend extends AbstractController
 {
+	private $session;
+		
+	public function __construct(SessionInterface $session) {
+		$this->session = $session;
+	}
 	/**
 	* @Route("/backend", name="catch") methods=("GET", "POST")
 	*/
 	public function index(SessionInterface $session)
-	{
+	{		
 		$request = Request::createFromGlobals(); //the envelope, and we're looking inside it
 		
 		$type = $request->request->get('type', 'none'); // to send ourselves in different directions
@@ -45,37 +51,96 @@ class Backend extends AbstractController
 		}
 		
 		else if($type == 'login') //if we had a login
-		{
-			
-			
+		{			
 			//get username and password
 			$username = $request->request->get('username', 'none');
-			$password = $request->request->get('password', 'none');
-			
-			
-		
+			$password = $request->request->get('password', 'none');		
 			
 			$repo = $this->getDoctrine()->getRepository(Login::class); //type of entityManager
 			
-			$person = $repo->findOneBy(['username' => $username, 'password' => $password,]);
-			
-			  
-                // save the user ID to the session
-                // KEY-VALUE
-                
-                // KEY - IS the name we give it
-                // $variable - is what we want to save.
-               $session->set('username', $username);
-			
-			
+			$person = $repo->findOneBy(['username' => $username, 'password' => $password]);
+
+			//$session->set('id', $repo->findOneBy(['username' => $username])->getId());	//set session id to the id of the user that has logged in		
+			$session->set('placedby', $username);
 			
 			return new Response($person->getAcctype());
 			
 		}
-		else if($type == 'getusername'){
-            // send back a response, with the username we stored in the session.
-            return new Response($session->get('username'));
+		
+		
+		
+		else if($type == 'vieworders') {
+			//$id = $session->get('id');
+			$placedby = $session->get('placedby');
+			
+			$string1 = "";
+			
+			$repository = $this->getDoctrine()->getRepository(Orders::class);
+            //$orders = $repository->findAll()->get($placedby);
+			
+			//foreach($orders as $order => $x) {
+			//	$string1 .= "<p>".$x."</p>";
+			//}
+		
+					
+			return new Response($placedby);
 		}
+		
+		else if($type == 'placeorder'){         
+        
+        // catch the variables we sent from the JavaScript.
+        $placedby = $session->get('placedby');
+        $ser = $request->request->get('ser', 'this is the default');
+        $address = $request->request->get('address', 'this is the default');
+		$status = $request->request->get('status', 'this is the default');
+      
+      
+      
+      
+      // Break apart the serialized order
+	  
+        //$data = explode('=', $ser); //this is what order details look like
+        
+        //foreach($data as $record) {    
+           
+
+       //     $item = explode('-',$record);
+        //    echo 'Item: ' . $item[0] . '<br>';
+        //    echo 'Qty: ' . $item[1] . '<br>';
+            
+     //   }
+    
+        
+        // to work the objects
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // create blank entity of type "Orders"
+        $order = new Orders();
+        
+        $order->setPlacedBy($placedby);
+       // $order->setDetails(substr($data, 0, -1));
+		$order->setDetails($ser);		//knocks last = sign off the end of description
+		$order->setAddress($address);
+		$order->setStatus($status);
+
+
+      
+        $entityManager->persist($order);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+ 
+
+       
+        return new Response(
+            'all ok' . $ser
+        );  
+		
+		}		
+        
+       
+    
 	}
+	
 }
 ?>
